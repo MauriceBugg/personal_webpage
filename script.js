d513482a77bd0488f0792e51b453058e92dd94f1
MauriceBugg <47580642+MauriceBugg@users.noreply.github.com>
2020-04-18 20:06:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Adds a random welcome message to welcome page.
+ */
+function getRandomQuoteUsingArrowFunctions(){
+    fetch('/data').then(response => response.json()).then((greetings) => { 
+        console.log(greetings[0]);
+        console.log(greetings[1]);
+    // Build the list of comment entries
+    const greetList = document.getElementById('greeting-container');
+    greetings.forEach((line) => {
+      greetList.appendChild(createListElement(line));
+    });
+  });
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+}
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
